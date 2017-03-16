3b1eaa3da7b74ec572a65dc7dd8218d80eb0a91b
Cymen Vig <cymenvig@gmail.com>
2017-03-16 16:01:42 -0500

    adds bable for es6 and gets "Hello world!" working for http root (/)

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import Koa from 'koa';
+
+const app = new Koa();
+
+// views
+import render from 'koa-ejs';
+render(
+  app,
+  {
+    cache: process.env.NODE_ENV === 'production',
+    root: __dirname + '/views',
+    viewExt: 'ejs',
+  }
+);
+
+// body parser
+import bodyParser from 'koa-bodyparser';
+app.use(bodyParser());
+
+// static files
+import serve from 'koa-static';
+app.use(serve('./public'));
+
+
+// normally, I'd split routes into multiple files but keeping in this one for demo
+import Router from 'koa-router';
+const router = new Router();
+
+router
+	.get('/', (ctx) => {
+		ctx.body = 'Hello world!';
+	});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+if (process.env.NODE_ENV === 'production') {
+	app.listen(8080, '127.0.0.1');
+	console.log('Listening on port 127.0.0.1:8080');
+} else {
+	app.listen(8080);
+	console.log('Listening on port 8080');
+}
+
+// cleanup on exit/restart
+process.on('SIGINT', () => {
+  // TODO require('./db/pg').end();
+  process.exit();
+});
