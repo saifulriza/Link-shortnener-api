15a88a48a1a77adac08562d2eaa1b7345e075234
saifulriza123 <mail.saifulriza@gmail.com>
2020-11-24 18:54:40 +0700

    add url validation

--- src/index.js
+++ src/index.js
@@ -1,63 +1,75 @@
-import Koa from 'koa';
+import Koa from "koa";
 const app = new Koa();
 
+const isURL = require("isurl");
+
 // database
-const pgPromise = require('pg-promise')();
+const pgPromise = require("pg-promise")();
 const db = pgPromise(process.env.DATABASE_URL);
 
 // create table if not present
 db.tx(async (t) => {
   try {
-    await t.none('CREATE EXTENSION IF NOT EXISTS citext;');
+    await t.none("CREATE EXTENSION IF NOT EXISTS citext;");
     await t.none(
       `CREATE TABLE IF NOT EXISTS data (
       url CITEXT,
       id CITEXT,
       PRIMARY KEY(id, url)
     )`
-    )
+    );
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-
 });
 
 // cors
-import cors from '@koa/cors'
+import cors from "@koa/cors";
 app.use(cors());
 
 // body parser
-import bodyParser from 'koa-bodyparser';
+import bodyParser from "koa-bodyparser";
 app.use(bodyParser());
 
 // normally, I'd split routes into multiple files but keeping in this one for demo
-import Router from 'koa-router';
-import shortId from 'shortid';
+import Router from "koa-router";
+import shortId from "shortid";
 
 const router = new Router();
 
 router
-  .post('/', async (ctx) => {
-    const { url } = ctx.request.body;
-    const row = await db.oneOrNone('SELECT id FROM data WHERE url = $1', url);
+  .post("/", async (ctx, res) => {
+    if (isURL(new URL(ctx.request.body))) {
+      const { url } = ctx.request.body;
+    } else {
+      ctx.body = {
+        status: 400,
+        message: "Url tidak valid",
+      };
+    }
+
+    const row = await db.oneOrNone("SELECT id FROM data WHERE url = $1", url);
     let id;
     if (row) {
       id = row.id;
     } else {
       id = shortId(url);
-      await db.none('INSERT INTO data (url, id) VALUES ($1, $2)', [url, id]);
+      await db.none("INSERT INTO data (url, id) VALUES ($1, $2)", [url, id]);
     }
 
     ctx.body = {
       id: id,
     };
   })
-  .get('/:id', async (ctx) => {
-    const row = await db.oneOrNone('SELECT url FROM data WHERE id = $1', ctx.params.id);
+  .get("/:id", async (ctx) => {
+    const row = await db.oneOrNone(
+      "SELECT url FROM data WHERE id = $1",
+      ctx.params.id
+    );
     if (row) {
       let url = row.url;
-      if (!url.startsWith('http')) {
-        url = 'http://' + url;
+      if (!url.startsWith("http")) {
+        url = "http://" + url;
       }
       ctx.redirect(url);
     } else {
@@ -68,11 +80,11 @@ router
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   const port = process.env.PORT || 8080;
   app.listen(port);
   console.log(`Listening on port ${port}`);
 } else {
   app.listen(8080);
-  console.log('Listening on port 8080');
+  console.log("Listening on port 8080");
 }
